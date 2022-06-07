77296d3c1490688cfb18f22ef2c56bb2ed3f7e07
James Fincher <heartandkarma@gmail.com>
2022-06-06 19:39:23 -0700

    testing navbar

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,14 @@ import Head from "next/head";
 // add bootstrap css
 import "bootstrap/dist/css/bootstrap.css";
 import "../styles/globals.scss";
-
+import Navbar from "../components/Navbar";
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+      <Navbar />
       <Component {...pageProps} />
     </>
   );
